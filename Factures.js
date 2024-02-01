2bd3602e5280e13baea2aac8935142ce1e9f0d01
pnavlani <p.navlani@sapalomera.cat>
2024-02-01 16:43:18 +0100

    Creacio de classe Factura

--- Factures.js
+++ Factures.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+let numero, data, nif, client,telefon,email,subtotal,dte,base
+function afegirFactura(){
+
+}
+
+
+function recuperarFactura(){
+
+}
+
+function guardarFactura(){
+
+}
+
+function init(){
+
+}
