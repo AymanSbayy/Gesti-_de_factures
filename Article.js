70124609eca442e90cd86a24c5bdc3b9482b6285
pnavlani <p.navlani@sapalomera.cat>
2024-02-02 16:27:13 +0100

    Classe Article

---
+++ Article.js
@@ -0,0 +1,16 @@
+ 
+export class Article {
+    codi;
+    article;
+    unitats;
+    preu;
+    subtotal;
+
+    constructor(codi,article,unitats,preu,subtotal){
+        this.codi = codi;
+        this.article = article;
+        this.unitats = unitats;
+        this.preu = preu;
+        this.subtotal = subtotal;
+    }
+}
